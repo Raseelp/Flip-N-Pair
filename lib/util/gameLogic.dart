d990a5e6590d629a18fd52092fc7431fb3e93732
Raseel <raseelp321@gmail.com>
2024-11-20 23:44:11 +0530

    Addeed new pictures

--- lib/util/gameLogic.dart
+++ lib/util/gameLogic.dart
@@ -2,24 +2,38 @@ class Game {
   final String hiddenCardPath = 'assets/images/hidden.png';
   final int cardCount;
   List<String>? gameImg;
-  final List<String> imagePath = [
-    'assets/images/1.png',
-    'assets/images/2.png',
-    'assets/images/3.png',
-    'assets/images/4.png',
-    'assets/images/5.png',
-    'assets/images/6.png',
-    'assets/images/7.png',
-    'assets/images/8.png',
-    'assets/images/9.png',
-    'assets/images/10.png',
+  final List<List<String>> themes = [
+    [
+      'assets/images/1.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+      'assets/images/4.png',
+      'assets/images/5.png',
+      'assets/images/6.png',
+      'assets/images/7.png',
+      'assets/images/8.png',
+      'assets/images/9.png',
+      'assets/images/10.png',
+    ],
+    [
+      'assets/images/c1.png',
+      'assets/images/c2.png',
+      'assets/images/c3.png',
+      'assets/images/c4.png',
+      'assets/images/c5.png',
+      'assets/images/c6.png',
+      'assets/images/c7.png',
+      'assets/images/c8.png',
+      'assets/images/c9.png',
+      'assets/images/c10.png',
+    ]
   ];
 
   List<String> cardsList = [];
 
   void generateImages(int gridSize) {
     int uniqueImageCount = gridSize ~/ 2;
-    final List<String> selectedImages = imagePath.sublist(0, uniqueImageCount);
+    final List<String> selectedImages = themes[1].sublist(0, uniqueImageCount);
     cardsList = [...selectedImages, ...selectedImages];
     cardsList.shuffle();
   }
