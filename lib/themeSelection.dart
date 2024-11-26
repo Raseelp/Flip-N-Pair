34d1729b18812cb5149f5a5e528f69976d52fcc9
Raseel <raseelp321@gmail.com>
2024-11-26 23:45:36 +0530

    .

--- lib/themeSelection.dart
+++ lib/themeSelection.dart
@@ -75,7 +75,7 @@ class ThemeSelection extends StatelessWidget {
                 padding: EdgeInsets.symmetric(
                     horizontal: screenWidth * 0.13,
                     vertical: screenHeight * 0.018),
-                child: const Text('Siuuuuuuu',
+                child: const Text('Gollllllllllllaaasoooo',
                     style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
